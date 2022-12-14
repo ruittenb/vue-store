921348795d2cf42b64f19896aae833d3025264c7
René Uittenbogaard <ruittenb@gmail.com>
2022-12-14 13:12:53 +0100

    Progress on Products{Adapter,Client}

--- src/adapters/ProductsAdapter.js
+++ src/adapters/ProductsAdapter.js
@@ -9,18 +9,37 @@ export default class ProductsAdapter {
         this.apiKey = import.meta.env.VITE_PRODUCTS_API_X_API_KEY;
     }
 
-    _getListUrl(pageNr, pageSize) {
-        return `${this.proto}://${this.host}${this.path}?page=${pageNr}&limit=${pageSize}`;
+    /**
+     * Gets the URL for fetching a page's worth of products.
+     *
+     * @return {String}
+     */
+    _getListUrl() {
+        return `${this.proto}://${this.host}${this.path}`;
     }
 
+    /**
+     * Fetches a list of products from the Products API
+     *
+     * @param {Number} pageNr - the page number to fetch (starts at 1)
+     * @param {Number} pageSize - the number of products per page
+     * @return {null|Response} - response object as returned by superagent,
+     *     or null in case of error (FIXME)
+     */
     async list(pageNr, pageSize) {
         try {
+            const url = this._getListUrl();
             return await superagent
-                .get(this._getListUrl(pageNr, pageSize))
+                .get(url)
+                .query({
+                    page: pageNr,
+                    limit: pageSize
+                })
                 .set('X-API-Key', this.apiKey) // unused by this API but we already support it
-                .set('accept', 'json');
+                .type('json')
+                .accept('json');
         } catch (error) {
-            // TODO
+            return null; // TODO
         }
     }
 }
--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,7 +1,16 @@
 <script setup>
-import ProductTile from "../components/ProductTile.vue";
+import ProductTile from '../components/ProductTile.vue';
+import ProductsClient from '../clients/ProductsClient';
+
+let productsClient = new ProductsClient();
+productsClient.list(1, 10).then((result) => {
+  console.log(result);
+});
+
 </script>
 
 <template>
   <ProductTile name="Accu boorhamer" />
+  {{
+  }}
 </template>

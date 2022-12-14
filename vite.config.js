38c98b85d21049e434d06e0fadbf882d7485588d
René Uittenbogaard <ruittenb@gmail.com>
2022-12-14 11:22:06 +0100

    Enabled vite source map; added .env

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,9 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+    }
   },
+  build: {
+    sourcemap: true
+  }
 });
